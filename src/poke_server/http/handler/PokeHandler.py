c97d243e0c0a9477b5444f00c2d1a38843a1c99f
Alex Diaz <alex@akdev.xyz>
2021-07-11 10:07:06 -0400

    experiment #2 - handler

---
+++ src/poke_server/http/handler/PokeHandler.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from logging import getLogger
+
+from poke_server.http.router import (
+    HTTPResponse
+)
+
+logger = getLogger(__name__)
+
+class PokeHandler:
+    @classmethod
+    def index(cls, path, body): # PokeHandler.index("/", "{}")
+        logger.info("poke-handler executing")
+        return HTTPResponse(
+            "hello poke",
+            200
+        )
+
+    @classmethod
+    def get_pokemon(cls, path, body): # PokeHandler.get_pokemon("/pokemon/1", "{}") => HTTPResponse
+        logger.info("fetching pokemon")
+        return HTTPResponse(
+            {"id": 1 },
+            200
+        )
+
+    @classmethod
+    def update_pokemon(cls, path, body):
+        logger.info("updating pokemon")
+        return HTTPResponse(
+            {"id": 1},
+            200
+        )
+
+    @classmethod
+    def get_and_update_pokemon(cls, path, body):
+        cls.get_pokemon(path, body)
+        cls.update_pokemon(path, body)
